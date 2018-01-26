7517a01fa5e7d92798ec39d74b1abdae7022b747
Melissa Bell <melissabell456@gmail.com>
2018-01-26 10:44:33 -0600

    added the ability to filter by price

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -7,5 +7,11 @@ angular.module("TravelGuideApp").controller("BookCtrl", function($scope, BookFac
     $scope.guides = travelGuides;
   });
 
+  $scope.displayBookString = "book in guides | orderBy: '' | filter: {type: bookType, title: titleSearch}";
+  $scope.sortPrices = () => {
+    $scope.displayBookString = "book in guides | orderBy: '-price' | filter: {type: bookType, title: titleSearch}";
+    // return "book in guides | orderBy: '-price' | filter: {type: bookType, title: titleSearch}";
+  };
+
   // $scope.books = <your array of guide books data>
 });
